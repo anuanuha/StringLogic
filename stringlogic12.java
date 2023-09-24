cfeec182c4f0ed4cae18d912b8d644eaedb7a675
ANU. H A <125720369+anuanuha@users.noreply.github.com>
2023-09-24 19:07:31 +0530

    remove numbers in a sentence

---
+++ stringlogic12.java
@@ -0,0 +1,9 @@
+package stringlogic;
+
+public class remove_numbers_in_string {
+	public static void main(String[] args) {
+	String str="234fdhmg 578ehjbvfhu jhui876e r967879";
+	System.out.println(str.replaceAll("[0-9]",""));
+
+}
+}
