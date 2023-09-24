a3a78d17008f5ceab23f80ff73cc618c45448dc4
ANU. H A <125720369+anuanuha@users.noreply.github.com>
2023-09-24 18:55:17 +0530

    reverse a even word in a given sentence

---
+++ stringlogic6.java
@@ -0,0 +1,24 @@
+
+package stringlogic;
+public class reversesentenceandstringodd{
+	    public static void main(String[] args) {
+	    
+	        String sentence = "hi hello java how are you"; // ih avaj era
+	        System.out.println(sentence);
+	        String[] words = sentence.split(" ");
+	        for (int i=0;i<=words.length-1;i++)
+	        {
+	        	if(i%2!=0)
+	        	{
+	        		String str2=words[i];//hi
+	        		String reverse="";
+	        	   for (int j=str2.length()-1;j>=0;j--)
+	        	   {
+	        		   reverse=reverse+str2.charAt(j);
+	        	   }
+
+	        	System.out.print(reverse+" ");
+	    }
+	    }
+	    }
+}
