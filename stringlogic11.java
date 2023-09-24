117e81d80927e1d62bff2d168d8cceda9a383365
ANU. H A <125720369+anuanuha@users.noreply.github.com>
2023-09-24 19:04:23 +0530

    remove extra space in sentence

---
+++ stringlogic11.java
@@ -0,0 +1,8 @@
+package stringlogic;
+
+public class removeextraspace {
+        public static void main(String[] args) {
+			String str="i           am                anu         H A";
+			System.out.println(str.replaceAll(" +"," "));
+		}
+}
